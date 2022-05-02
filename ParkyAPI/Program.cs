8d29badd2c1d4f46aa072a8ec67bd7a10fffe4c4
Reza Ashena <Ashena.reza@gmail.com>
2022-05-02 09:37:32 -0400

    Add User Repository

--- ParkyAPI/Program.cs
+++ ParkyAPI/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
     ));
 builder.Services.AddScoped<INationalParkRepository, NationalParkRepository>();
 builder.Services.AddScoped<ITrailRepository, TrailRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddAutoMapper(typeof(ParkyMappings));
 
 var app = builder.Build();
