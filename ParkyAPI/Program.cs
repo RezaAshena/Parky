791304ce9f77a18d012c59202e2b29e06f10dca5
Reza Ashena <Ashena.reza@gmail.com>
2022-05-02 09:57:32 -0400

    Configuring Secret Key

--- ParkyAPI/Program.cs
+++ ParkyAPI/Program.cs
@@ -4,6 +4,7 @@ using ParkyAPI.Repository;
 using ParkyAPI.Repository.IRepository;
 using AutoMapper;
 using ParkyAPI.ParkyMapper;
+using ParkyAPI;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +18,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
+
+var appSettings = builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
+
 builder.Services.AddScoped<INationalParkRepository, NationalParkRepository>();
 builder.Services.AddScoped<ITrailRepository, TrailRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
